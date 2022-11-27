fc452619fb78f768cdfa25789d7894739f188ba3
Maciej Pędzich (Mac) <contact@maciejpedzi.ch>
2022-11-27 19:03:25 +0100

    Update ItemSelect.vue

--- src/components/ItemSelect.vue
+++ src/components/ItemSelect.vue
@@ -1,12 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { useItemComparison } from '../composables/itemComparison'
 
-const selected = ref()
+const { isFetchingItems, availableItems, itemsToCompare } = useItemComparison()
+
+const selectedItem = ref()
+
+watch(selectedItem, (newItem, prevItem) => {
+  itemsToCompare.value = itemsToCompare.value.filter(
+    item => item.id !== prevItem?.id
+  )
+  itemsToCompare.value.push(newItem)
+})
 </script>
 
 <template>
-  <select v-model="selected" class="p-2 border-2 border-gray-dark">
+  <p v-if="isFetchingItems">Please wait...</p>
+  <select v-else v-model="selectedItem" class="p-2 border-2 border-gray-dark">
     <option disabled value="">Select an item</option>
+    <option v-for="item in availableItems" :key="item.id" :value="item">
+      {{ item.title }} - ${{ item.price }}
+    </option>
   </select>
 </template>
